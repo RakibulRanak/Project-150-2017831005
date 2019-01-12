247fd1673442021a5914954e2d5b1a347a647bd1
RakibulRanak <rakibulhasanranak1@gmail.com>
2019-01-12 23:01:31 +0600

    problems
    done

--- Type 2/DFS/dfs implementation.cpp
+++ Type 2/DFS/dfs implementation.cpp
@@ -3,35 +3,22 @@ using namespace std;
 int table[100][100]={0};
 int edge_id[100];
 int visited[100]={0};
-int E,N;
-stack<int>S;
 //s is starting vartex
 //n is number of vertices
 void dfs(int s,int n)
 {
-    S.push(s);
     cout<<s<<" ";
-    while(!S.empty())
-    {
-        int u=S.top();
-        visited[u]=1;
-        S.pop();
-		for(int i=0;i<E;i++)
+    visited[s]=1;
+		for(int i=0;i<n;i++)
 		{
-			if(table[u][i]==1&&visited[i]==0)
+			if(table[s][i]==1&&visited[i]==0)
 			{
-				visited[i]=1;
-				S.push(i);
-				cout<<i<<" " ;
-				break;
+				//cout<<i<<" " ;
+				dfs(i,n);
 			}
 		}
 
-    }
-    for(int i=0;i<N;i++){
-        if(visited[i]==0)
-            dfs(i,N);
-    }
+
 }
 
 
@@ -41,10 +28,8 @@ int main()
     int n,e;
     cout<<"Number of vertices/nodes : " <<endl;
     cin>>n;
-    N=n;
     cout<<"Number of edges: "<<endl;
     cin>>e;
-    E=e;
     int n1,n2;
     for(int i=0;i<e;i++)
     {
@@ -52,16 +37,22 @@ int main()
         table[n1][n2]=1;
 
     }
-    for(int i=0;i<e;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<e;j++)
+        for(int j=0;j<n;j++)
             cout<<table[i][j]<<" ";
         cout<<endl;
     }
     cout<<"Enter the vertice you want to start dfs: "<<endl;
-    int N;
-    cin>>N;
-    dfs(N,n);
+    int p;
+    cin>>p;
+    dfs(p,n);
+
+    for(int i=0;i<n;i++){
+        if(visited[i]==0)
+            dfs(i,n);
+    }
     return 0;
 
 }
+
